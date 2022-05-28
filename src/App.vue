40834d2b2691a96b592ec086fcdf20c97dbe813e
Meghant00 <meghant@reconwithme.com>
2022-05-28 16:03:40 +0545

    Feat: Add Sidebar

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,19 @@
-<script setup></script>
-
 <template>
-  <router-view />
+  <div class="flex flex-row items-start justify-start gap-4">
+    <div class="w-[15%] h-screen overflow-hidden">
+      <side-bar />
+    </div>
+    <div class="w-[85%] px-4 py-8">
+      <router-view />
+    </div>
+  </div>
 </template>
+
+<script>
+import SideBar from "@/components/sidebar/SideBar.vue";
+import { defineComponent } from "vue";
+export default defineComponent({
+  setup() {},
+  components: { SideBar },
+});
+</script>
---
+++ src/components/sidebar/SideBar.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="w-full h-full border-r border-gray-300 py-4">
+    <n-menu :options="menuOptions" class="text-base" />
+  </div>
+</template>
+<script>
+import { defineComponent } from "vue";
+import { NMenu } from "naive-ui";
+const menuOptions = [
+  {
+    label: "Dashboard",
+    key: "dashboard",
+  },
+  {
+    label: "Boid",
+    key: "boid",
+  },
+  {
+    label: "Company",
+    key: "company",
+  },
+];
+export default defineComponent({
+  setup() {
+    return { menuOptions };
+  },
+  components: { NMenu },
+});
+</script>
